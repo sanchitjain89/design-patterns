5e619247f4e9de2b356d64f62763ba550fad032d
Sanchit <sanchitjain89@gmail.com>
2025-04-29 12:47:50 +0530

    Command Pattern changes

--- src/command/Light.java
+++ src/command/Light.java
@@ -2,15 +2,12 @@ package command;
 
 // Receiver
 class Light {
-    private boolean isOn = false;
 
     void turnOn() {
-        isOn = true;
         System.out.println("Light is ON");
     }
 
     void turnOff() {
-        isOn = false;
         System.out.println("Light is OFF");
     }
 }
